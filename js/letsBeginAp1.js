54f3a4a3e40b355dd1c6581adbaae0684eccc0a1
blog1916 <546489979@qq.com>
2017-05-12 09:01:57 +0800

    Site updated: 2017-05-12 09:01:55

---
+++ js/letsBeginAp1.js
@@ -0,0 +1,12 @@
+var ap1 = new APlayer({
+	element: document.getElementById('player1'),
+	narrow: false,
+	autoplay: true,
+	music: {
+		title: '彩虹',
+		author: '周杰伦',
+		url: 'http://onom4m8zz.bkt.clouddn.com/%E5%91%A8%E6%9D%B0%E4%BC%A6%20-%20%E5%BD%A9%E8%99%B9.mp3',
+		pic: 'http://p1.music.126.net/0tWIPPsjYexJOvRqazMw6A==/60473139533046.jpg'
+	}
+});
+ap1.init();
